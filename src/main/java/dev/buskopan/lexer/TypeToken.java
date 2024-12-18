4c0976a451fbb06494bc422b81d9cfd9637c6975
= <=>
2024-12-18 18:18:47 -0300

    create types of token

---
+++ src/main/java/dev/buskopan/lexer/TypeToken.java
@@ -0,0 +1,60 @@
+package dev.buskopan.lexer;
+
+public enum TypeToken {
+    INICIO_OBJETO,
+    FIM_OBJETO,
+    DOIS_PONTO,
+    VIRGULA,
+    TEXTO,
+    NUMERO,
+    INICIO_ARRAY,
+    FIM_ARRAY,
+    BOOLEANO,
+    NULO;
+}
+
+//{
+// "
+// n
+// o
+// m
+// e
+// "
+// :
+// "
+// a
+// n
+// d
+// r
+// e
+// "
+// ,
+//"
+// i
+// d
+// a
+// d
+// e
+// "
+// :
+// 1
+// 2
+// ,
+// "
+// a
+// d
+// u
+// l
+// t
+// o
+// "
+// :
+// f
+// a
+// l
+// s
+// e
+// ,
+
+//    "cnh": null
+//        }
